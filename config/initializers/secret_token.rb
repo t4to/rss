320a378eb92e92813ba4aec37619e4f887300bce
Tato <tato.rodrigues@gmail.com>
2011-06-01 10:46:56 -0300

    estrutura

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RSS::Application.config.secret_token = '2fd05da4b2062ca4c6f4307c97f7988abd90e4acdd30e61fe1696e013133e7c26934349c5e10aa571b6180c9d73c820679bbdbad20148644d97097ff33ceed82'
