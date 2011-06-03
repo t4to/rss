d8c9162d890bf0d9d07a23397949fca87da60989
Tato <tato.rodrigues@gmail.com>
2011-06-03 15:39:36 -0300

    Layout-Events OK

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
   require 'rss'
+  require 'twitter'
   def index
+	#Notícias
 	@rss = RSS::Parser.parse(open('http://www.vnews.com.br/cache_rss/rss.xml').read, false)	
+	
+	
+	#Twitter
+	screen_name = 'sitevnews'
+
+	#Recuperar informações de usuário
+	@user = Twitter.user(screen_name)
+
+	#Recuperar timeline
+	search = Twitter::Search.new
+	@resp = Twitter.user_timeline(screen_name, :count => 20)
+
+	search.clear
   end
 
 end
