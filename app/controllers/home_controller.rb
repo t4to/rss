e4d90a9ecdef35ce4305ee59edcffb26a00f9950
Tato <tato.rodrigues@gmail.com>
2011-06-03 11:01:31 -0300

    Ajustes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   require 'rss'
   def index
-	@rss = RSS::Parser.parse(open('http://www.vnews.com.br/cache_rss/rss.xml').read, false)
+	@rss = RSS::Parser.parse(open('http://www.vnews.com.br/cache_rss/rss.xml').read, false)	
   end
 
 end
